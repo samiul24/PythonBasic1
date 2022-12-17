26217e582bc27d611ef52e3500d4865dec7e2a5e
Md Samiul Islam <samiul24>
2022-12-17 22:18:56 +0600

    Third

--- InstanceMethod.py
+++ InstanceMethod.py
@@ -8,7 +8,8 @@ s = Student('Samiul', 32)
 print(s.name)
 
 s1 = Student('Sami', 32)
-print(s1.name)
+s1.dob = 1992 # add new instance variable 
+print(f"Name: {s1.name}, Age: {s1.age}, DOB: {s1.dob}")
 
 
 class Teacher:
@@ -22,3 +23,24 @@ class Teacher:
 
 t = Teacher('Saddam', 'ICT')
 t.display()
+
+
+class Student1:
+    # constructor
+    def __init__(self, name, age):
+        # Instance variable
+        self.name = name
+        self.age = age
+
+# create object
+stud = Student1("Jessa", 20)
+
+print('Before')
+print('Name:', stud.name, 'Age:', stud.age)
+
+# modify instance variable
+stud.name = 'Emma'
+stud.age = 15
+
+print('After')
+print('Name:', stud.name, 'Age:', stud.age)
---
+++ InstanceMethod1.py
@@ -0,0 +1,25 @@
+class Vehicle:
+    def __init__(self, max_speed) -> None:
+        self.max_speed = max_speed
+
+
+class Car(Vehicle):
+    def __init__(self, engine, max_speed) -> None:
+        super().__init__(max_speed)
+        self.engine = engine
+
+    def display(self):
+        print(self.max_speed)
+
+v = Vehicle('3000 Km/h')
+c = Car('Toyota', '3000 Km/h')
+c.display()
+
+iv = c.__dict__
+print(iv)
+
+for key, value in iv.items():
+    print(f"Key: {key}, Value: {value}")
+
+for key_value in iv.items():
+    print(f"Key: {key_value[0]}, Value: {key_value[1]}")
